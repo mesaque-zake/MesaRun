f8269233d88af4c30d8a4d73ba25e965b73a890e
Mesaque <imesalink@gmail.com>
2026-07-13 13:34:33 -0300

    Set up game initialization and rendering loop
    Initialize the game engine and set up the game loop.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-//
+import { initEngine, render } from './engine.js';
+
+function init() {
+    console.log("Iniciando o MesaRun!...");
+    
+    // 1. Liga o motor 3D
+    initEngine();
+    
+    // 2. Some com o texto de carregamento
+    const loadingText = document.getElementById('loading-text');
+    if(loadingText) {
+        loadingText.style.display = 'none';
+    }
+
+    // 3. Dispara o Loop do Jogo (60 frames por segundo)
+    gameLoop();
+}
+
+function gameLoop() {
+    // Pede ao navegador para chamar essa função de novo no próximo frame
+    requestAnimationFrame(gameLoop);
+    
+    // No futuro, chamaremos a atualização do caminhão e trânsito aqui
+    
+    // Manda a engine desenhar tudo na tela
+    render();
+}
+
+// Garante que a página web carregou antes de disparar o código
+window.onload = init;
